a81265ab1652a378a28424dcefc01291526b6662
Tran Nguyen Hoang Loc <hoangloc1307@gmail.com>
2026-06-24 11:09:40 +0700

    chore(fe): :hammer: configure HTTPS for Vite and update pnpm version

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import fs from 'node:fs';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   server: {
-    port: 3000,
-  }
-})
+    host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../cert/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../cert/localhost.pem')),
+    },
+  },
+});
