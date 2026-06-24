c82b181b6e62eda2c26f6b0fb0f95fd19f769a01
Tran Nguyen Hoang Loc <hoangloc1307@gmail.com>
2026-06-24 11:45:09 +0700

    chore: :hammer: update lint-staged config

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,8 +1,8 @@
 import type { Configuration } from 'lint-staged';
 
 const lintStagedConfig: Configuration = {
-  'fe/**/*.{js,jsx,ts,tsx,cjs,mjs}': ['pnpm -C fe eslint --fix'],
-  'be/**/*.{js,jsx,ts,tsx,cjs,mjs}': ['pnpm -C be eslint --fix'],
+  'fe/**/*.{js,jsx,ts,tsx,cjs,mjs}': ['pnpm --filter itam-fe exec eslint --fix'],
+  'be/**/*.{js,jsx,ts,tsx,cjs,mjs}': ['pnpm --filter itam-be exec eslint --fix'],
   '*.{js,jsx,ts,tsx,cjs,mjs,json,md,html,css,scss}': ['prettier --write'],
 };
 
