9247f1d19ca5bd400976af78832fe6eaf7f08235
Tran Nguyen Hoang Loc <hoangloc1307@gmail.com>
2026-06-24 14:18:50 +0700

    build(fe): :package: add tailwindcss, shadcn ui

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -3,9 +3,15 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import fs from 'node:fs';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     host: '0.0.0.0',
     https: {
